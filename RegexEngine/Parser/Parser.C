d736720ea826f6c6ba14883e887363a498e85081
mihlali dlulane <midlulajhb024@student.wethinkcode.co.za>
2025-09-10 14:14:53 +0200

    decided to approach regex differently

--- RegexEngine/Parser/Parser.C
+++ RegexEngine/Parser/Parser.C
@@ -1,73 +1,178 @@
 #include "Parser.H"
-#include <cstdlib>
+#include <cstddef>
 #include <cstring>
-#include <stdio.h>
 
-
-bool regexChecker(const char *regex) {
-
-  
-  char stack[100];
-  int top = -1;
-  char currentChar;
-
-  for (int i = 0; regex[i] != '\0'; i++) {
-    currentChar = regex[i];
-
-    if (i == 0) {
-      if (currentChar == '*' || currentChar == '|') {
-        return false;
-      }
-    }
-
-    if (currentChar == '*' || currentChar == '|') {
-      if (regex[i + 1] == '*' || regex[i + 1] == '|' || regex[i + 1] == '(') {
-        return false;
-      }
-    }
-
-    if (currentChar == '(') {
-
-      if (regex[i + 1] == '*' || regex[i + 1] == '|') {
-        return false;
-      }
-
-      if (top >= 99) {
-        printf("Stack Overflow On Parantheses Balance Check\n");
-        return false;
-      }
-      stack[++top] = currentChar;
-
-    } else if (currentChar == ')') {
-
-      if (top == -1) {
-        return false; // Unbalanced: closing parenthesis without opening
-      }
-
-      char poppedChar;
-      if (top < 0) {
-        poppedChar = '\0'; // Indicate Empty Stack
-      } else {
-        poppedChar = stack[top--];
-      }
-      if ((currentChar == ')' && poppedChar != '(')) {
-        return false; // Unbalanced: mismatching parentheses
-      }
-    }
+RegexTokenType tokenChecker(char token) {
+  if (token == '(') {
+    return OPEN_P;
   }
 
-  if (top != -1) {
-    return false;
+  if (token == ')') {
+    return CLOSE_P;
   }
 
-  char lastChar = regex[strlen(regex) - 1];
-  if (lastChar == '|' || lastChar == '*') {
-    return false;
+  if (token == '*') {
+    return STAR;
   }
 
-    return true;
+  if (token == '+') {
+    return PLUS;
+  }
+
+  if (token == '|') {
+    return PIPE;
+  }
+
+  if (token == '?') {
+    return QUESTION;
+  }
+
+  if (token == '[') {
+    return OPEN_BRACKET;
+  }
+
+  if (token == ']') {
+    return CLOSE_BRACKET;
+  }
+
+  if (token == '.') {
+    return DOT;
+  }
+
+  if (token == '\\') {
+    return BACKLASH;
+  }
+
+  return LITERAL;
 }
 
-int countTokens(const char *regex) {}
+void tokeniser(const char *regex, token *ResultTokenArray) {
+  int assignCount = 0;
+  bool insideCharClass = false;
+  for (int i = 0; regex[i] != '\0'; i++) {
+    char currentChar = regex[i];
+    char nextChar;
+    RegexTokenType currentCharType = tokenChecker(currentChar);
 
-void tokeniser(const char *regex, token *ResultTokenArray) {}
+    if (currentCharType == BACKLASH) {
+      if (regex[i + 1] != '\0') {
+        nextChar = regex[i + 1];
+      }
+      i++;
+    }
+    if (currentCharType == OPEN_BRACKET) {
+      insideCharClass = true;
+    }
+
+    if (currentCharType == CLOSE_BRACKET) {
+      insideCharClass = false;
+    }
+
+    if (i == 0) {
+      if (currentChar == '^') {
+        currentCharType = START_ANCHOR;
+      }
+    }
+
+    if (i > 0) {
+      if (currentChar == '^') {
+        if (regex[i - 1] == '(' || regex[i - 1] == '|') {
+          currentCharType = START_ANCHOR;
+        }
+      }
+    }
+
+    if (i == strlen(regex) - 1) {
+      if (currentChar == '$') {
+        currentCharType = END_ANCHOR;
+      }
+    }
+
+    struct token t1;
+    t1.character = currentChar;
+    t1.tokenType = currentCharType;
+    t1.position = i;
+    t1.insideCharacterClass = insideCharClass;
+
+    if (currentCharType == BACKLASH) {
+      t1.escapedChar = nextChar;
+    }
+
+    ResultTokenArray[assignCount] = t1;
+    assignCount++;
+  }
+}
+
+// bool regexChecker(const char *regex) {
+
+// if (strlen(regex) == 0)
+//   return true;
+
+// char stack[100];
+// int top = -1;
+// char currentChar;
+
+// for (int i = 0; regex[i] != '\0'; i++) {
+//   currentChar = regex[i];
+
+//   if (i == 0) {
+//     if (currentChar == '*' || currentChar == '|') {
+//       return false;
+//     }
+//   }
+
+//   if (regex[i + 1] != '\0') {
+//     if (currentChar == '*' || currentChar == '|') {
+//       if (regex[i + 1] == '*' || regex[i + 1] == '|' || regex[i + 1] == '(')
+//       {
+//         return false;
+//       }
+//     }
+//   }
+
+//   if (currentChar == '(') {
+
+//     if (regex[i + 1] != '\0') {
+//       if (regex[i + 1] == '*' || regex[i + 1] == '|' || regex[i + 1] == ')')
+//       {
+//         return false;
+//       }
+//     }
+
+//     if (top >= 99) {
+//       printf("Stack Overflow On Parantheses Balance Check\n");
+//       return false;
+//     }
+//     stack[++top] = currentChar;
+
+//   } else if (currentChar == ')') {
+
+//     if (i > 0) {
+//       if (regex[i - 1] == '|') {
+//         return false;
+//       }
+//     }
+
+//     if (regex[i + 1] != '\0') {
+//       if (regex[i + 1] == '(') {
+//         return false;
+//       }
+//     }
+
+//     if (top < 1 || stack[top--] != '(') {
+//       return false;
+//     }
+//   }
+// }
+
+// if (top != -1) {
+//   return false;
+// }
+
+// char lastChar = regex[strlen(regex) - 1];
+// if (lastChar == '|' || lastChar == '*') {
+//   return false;
+// }
+
+// return true;
+//}
